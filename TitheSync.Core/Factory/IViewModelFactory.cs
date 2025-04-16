fcd5a4aad7adf1ccc293c49397f4d4887ca09d49
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-04-16 00:48:53 +0000

    Add IViewModelFactory interface and ViewModelFactory implementation for ViewModel creation

---
+++ TitheSync.Core/Factory/IViewModelFactory.cs
@@ -0,0 +1,11 @@
+﻿using MvvmCross.ViewModels;
+
+namespace TitheSync.Core.Factory
+{
+    public interface IViewModelFactory
+    {
+        TViewModel? CreateViewModel<TViewModel>() where TViewModel : MvxViewModel;
+
+        TViewModel? CreateViewModel<TViewModel, TParameter>( TParameter parameter ) where TViewModel : MvxViewModel;
+    }
+}
---
+++ TitheSync.Core/Factory/ViewModelFactory.cs
@@ -0,0 +1,22 @@
+﻿using MvvmCross;
+using MvvmCross.ViewModels;
+
+namespace TitheSync.Core.Factory
+{
+    public class ViewModelFactory:IViewModelFactory
+    {
+        public TViewModel? CreateViewModel<TViewModel>() where TViewModel : MvxViewModel => Mvx.IoCProvider?.Resolve<TViewModel>();
+
+        public TViewModel? CreateViewModel<TViewModel, TParameter>( TParameter parameter ) where TViewModel : MvxViewModel
+        {
+            TViewModel? viewModel = Mvx.IoCProvider?.Resolve<TViewModel>();
+
+            if (viewModel is IMvxViewModel<TParameter> parameterizedViewModel)
+            {
+                parameterizedViewModel.Prepare(parameter);
+            }
+
+            return viewModel;
+        }
+    }
+}
