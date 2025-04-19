38ac1d8d076e409dc07df5ce5e3f79f1fe8b2221
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-04-19 15:38:44 +0000

    Add enums for Bible classes and organizations to enhance domain model

---
+++ TitheSync.Domain/Models/Member.cs
@@ -0,0 +1,74 @@
+﻿using TitheSync.Domain.Enums;
+
+namespace TitheSync.Domain.Models
+{
+    /// <summary>
+    ///     Represents a member with personal and organizational details.
+    /// </summary>
+    /// <param name="memberId" >The unique identifier for the member.</param>
+    /// <param name="firstName" >The first name of the member.</param>
+    /// <param name="lastName" >The last name of the member.</param>
+    /// <param name="contact" >The contact information of the member.</param>
+    /// <param name="isLeader" >Indicates whether the member is a leader.</param>
+    /// <param name="address" >The address of the member.</param>
+    /// <param name="organization" >The organization the member belongs to.</param>
+    /// <param name="bibleClass" >The Bible class the member is part of.</param>
+    public class Member(
+        int memberId,
+        string firstName,
+        string lastName,
+        string contact,
+        bool isLeader,
+        string address,
+        OrganizationEnums organization,
+        BibleClassEnum bibleClass )
+    {
+        /// <summary>
+        ///     Gets the unique identifier for the member.
+        /// </summary>
+        public int MemberId { get; init; } = memberId;
+
+        /// <summary>
+        ///     Gets the first name of the member.
+        /// </summary>
+        public string FirstName { get; init; } = firstName;
+
+        /// <summary>
+        ///     Gets the last name of the member.
+        /// </summary>
+        public string LastName { get; init; } = lastName;
+
+        /// <summary>
+        ///     Gets the contact information of the member.
+        /// </summary>
+        public string Contact { get; init; } = contact;
+
+        /// <summary>
+        ///     Gets a value indicating whether the member is a leader.
+        /// </summary>
+        public bool IsLeader { get; init; } = isLeader;
+
+        /// <summary>
+        ///     Gets the address of the member.
+        /// </summary>
+        public string Address { get; init; } = address;
+
+        /// <summary>
+        ///     Gets the organization the member belongs to.
+        /// </summary>
+        public OrganizationEnums Organization { get; init; } = organization;
+
+        /// <summary>
+        ///     Gets the Bible class the member is part of.
+        /// </summary>
+        public BibleClassEnum BibleClass { get; init; } = bibleClass;
+
+        /// <summary>
+        ///     Gets or sets a value indicating whether the member is selected.
+        /// </summary>
+        public bool IsSelected { get; set; } = false;
+
+        public override string ToString() =>
+            $"MemberId: {MemberId}, Firstname: {FirstName}, Lastname: {LastName}, Contact: {Contact}, IsLeader: {IsLeader}, Address: {Address}, Organization: {Organization}, BibleClass: {BibleClass}";
+    }
+}
---
+++ TitheSync.Domain/Models/Payment.cs
@@ -0,0 +1,31 @@
+﻿namespace TitheSync.Domain.Models
+{
+    /// <summary>
+    ///     Represents a payment transaction.
+    /// </summary>
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="Payment" /> class.
+    /// </summary>
+    /// <param name="paymentId" >The unique identifier for the payment.</param>
+    /// <param name="amount" >The amount of the payment.</param>
+    /// <param name="datePaid" >The date the payment was made.</param>
+    public class Payment( int paymentId, decimal amount, DateOnly datePaid )
+    {
+        /// <summary>
+        ///     Gets the unique identifier for the payment.
+        /// </summary>
+        public int PaymentId { get; init; } = paymentId;
+
+        /// <summary>
+        ///     Gets the amount of the payment.
+        /// </summary>
+        public decimal Amount { get; init; } = amount;
+
+        /// <summary>
+        ///     Gets the date the payment was made.
+        /// </summary>
+        public DateOnly DatePaid { get; init; } = datePaid;
+
+        public override string ToString() => $"{nameof(PaymentId)}: {PaymentId}, {nameof(Amount)}: {Amount}, {nameof(DatePaid)}: {DatePaid}";
+    }
+}
