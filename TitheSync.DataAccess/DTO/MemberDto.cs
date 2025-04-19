9a9f681012caa53185d1f2314f2f797eebdc2169
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-04-19 15:37:23 +0000

    Add MemberDto and PaymentDto records for data transfer objects

---
+++ TitheSync.DataAccess/DTO/MemberDto.cs
@@ -0,0 +1,23 @@
+﻿namespace TitheSync.DataAccess.DTO
+{
+    /// <summary>
+    ///     Represents a data transfer object for a member.
+    /// </summary>
+    /// <param name="MemberId" >The unique identifier of the member.</param>
+    /// <param name="FirstName" >The first name of the member.</param>
+    /// <param name="LastName" >The last name of the member.</param>
+    /// <param name="Contact" >The contact information of the member.</param>
+    /// <param name="IsLeader" >Indicates whether the member is a leader.</param>
+    /// <param name="Address" >The address of the member.</param>
+    /// <param name="Organization" >The organization the member belongs to.</param>
+    /// <param name="BibleClass" >The Bible class the member attends.</param>
+    public record MemberDto(
+        int MemberId,
+        string FirstName,
+        string LastName,
+        string Contact,
+        bool IsLeader,
+        string Address,
+        string Organization,
+        string BibleClass );
+}
---
+++ TitheSync.DataAccess/DTO/PaymentDto.cs
@@ -0,0 +1,13 @@
+﻿namespace TitheSync.DataAccess.DTO
+{
+    /// <summary>
+    ///     Represents a payment data transfer object.
+    /// </summary>
+    /// <param name="PaymentId" >The unique identifier for the payment.</param>
+    /// <param name="Amount" >The amount of the payment.</param>
+    /// <param name="DatePaid" >The date the payment was made.</param>
+    public record PaymentDto(
+        int PaymentId,
+        decimal Amount,
+        DateTime DatePaid );
+}
