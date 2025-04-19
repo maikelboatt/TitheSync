f8d3b0f7e9fc333c3a194f1f7e755751a624151d
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-04-19 18:54:44 +0000

    Add ISqlDataAccess interface and SqlDataAccess implementation for SQL operations

---
+++ TitheSync.DataAccess/DatabaseAccess/ISqlDataAccess.cs
@@ -0,0 +1,32 @@
+﻿namespace TitheSync.DataAccess.DatabaseAccess
+{
+    /// <summary>
+    ///     Interface for SQL data access operations.
+    /// </summary>
+    public interface ISqlDataAccess
+    {
+        /// <summary>
+        ///     Executes a query asynchronously and returns the result as an enumerable of type <typeparamref name="T" />.
+        /// </summary>
+        /// <typeparam name="T" >The type of the result set.</typeparam>
+        /// <typeparam name="TU" >The type of the parameters object.</typeparam>
+        /// <param name="storedProcedure" >The name of the stored procedure to execute.</param>
+        /// <param name="parameters" >The parameters to pass to the stored procedure.</param>
+        /// <param name="connectionString" >The connection string to use. Defaults to "DefaultConnection".</param>
+        /// <returns>
+        ///     A task representing the asynchronous operation, containing the result set as an enumerable of type
+        ///     <typeparamref name="T" />.
+        /// </returns>
+        Task<IEnumerable<T>> QueryAsync<T, TU>( string storedProcedure, TU parameters, string connectionString = "DefaultConnection" );
+
+        /// <summary>
+        ///     Executes a command asynchronously without returning a result.
+        /// </summary>
+        /// <typeparam name="T" >The type of the parameters object.</typeparam>
+        /// <param name="storedProcedure" >The name of the stored procedure to execute.</param>
+        /// <param name="parameters" >The parameters to pass to the stored procedure.</param>
+        /// <param name="connectionString" >The connection string to use. Defaults to "DefaultConnection".</param>
+        /// <returns>A task representing the asynchronous operation.</returns>
+        Task CommandAsync<T>( string storedProcedure, T parameters, string connectionString = "DefaultConnection" );
+    }
+}
---
+++ TitheSync.DataAccess/DatabaseAccess/SqlDataAccess.cs
@@ -0,0 +1,21 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace TitheSync.DataAccess.DatabaseAccess
+{
+    public class SqlDataAccess:ISqlDataAccess
+    {
+        public async Task<IEnumerable<T>> QueryAsync<T, TU>( string storedProcedure, TU parameters, string connectionString = "DefaultConnection" )
+        {
+            using IDbConnection connection = new SqlConnection(connectionString);
+            return await connection.QueryAsync<T>(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+        }
+
+        public async Task CommandAsync<T>( string storedProcedure, T parameters, string connectionString )
+        {
+            using IDbConnection connection = new SqlConnection(connectionString);
+            await connection.ExecuteAsync(storedProcedure, parameters, commandType: CommandType.StoredProcedure);
+        }
+    }
+}
