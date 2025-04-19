a8f706643e1526827e5da9f1ad50df56308992f4
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-04-19 18:52:16 +0000

    Add DateConverterService and IDateConverterService for date conversions

---
+++ TitheSync.Domain/Services/DateConverterService.cs
@@ -0,0 +1,22 @@
+﻿namespace TitheSync.Domain.Services
+{
+    /// <summary>
+    ///     Service for converting between <see cref="DateTime" /> and <see cref="DateOnly" />.
+    /// </summary>
+    public class DateConverterService:IDateConverterService
+    {
+        /// <summary>
+        ///     Converts a <see cref="DateTime" /> to a <see cref="DateOnly" />.
+        /// </summary>
+        /// <param name="dateTime" >The <see cref="DateTime" /> to convert.</param>
+        /// <returns>A <see cref="DateOnly" /> representing the date part of the input.</returns>
+        public DateOnly ConvertToDateOnly( DateTime dateTime ) => new(dateTime.Year, dateTime.Month, dateTime.Day);
+
+        /// <summary>
+        ///     Converts a <see cref="DateOnly" /> to a <see cref="DateTime" />.
+        /// </summary>
+        /// <param name="dateOnly" >The <see cref="DateOnly" /> to convert.</param>
+        /// <returns>A <see cref="DateTime" /> representing the input date at midnight.</returns>
+        public DateTime ConvertToDateTime( DateOnly dateOnly ) => new(dateOnly.Year, dateOnly.Month, dateOnly.Day);
+    }
+}
---
+++ TitheSync.Domain/Services/IDateConverterService.cs
@@ -0,0 +1,22 @@
+﻿namespace TitheSync.Domain.Services
+{
+    /// <summary>
+    ///     Provides methods to convert between <see cref="DateTime" /> and <see cref="DateOnly" />.
+    /// </summary>
+    public interface IDateConverterService
+    {
+        /// <summary>
+        ///     Converts a <see cref="DateTime" /> to a <see cref="DateOnly" />.
+        /// </summary>
+        /// <param name="dateTime" >The <see cref="DateTime" /> to convert.</param>
+        /// <returns>The converted <see cref="DateOnly" />.</returns>
+        DateOnly ConvertToDateOnly( DateTime dateTime );
+
+        /// <summary>
+        ///     Converts a <see cref="DateOnly" /> to a <see cref="DateTime" />.
+        /// </summary>
+        /// <param name="dateOnly" >The <see cref="DateOnly" /> to convert.</param>
+        /// <returns>The converted <see cref="DateTime" />.</returns>
+        DateTime ConvertToDateTime( DateOnly dateOnly );
+    }
+}
