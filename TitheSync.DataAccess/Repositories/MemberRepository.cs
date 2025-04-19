4251b4e1cc352b936e2402c8c34c6b3d5e0be291
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-04-19 18:54:15 +0000

    Add IMemberRepository and IPaymentRepository interfaces with implementations for member and payment data management

---
+++ TitheSync.DataAccess/Repositories/MemberRepository.cs
@@ -0,0 +1,117 @@
+﻿using TitheSync.DataAccess.DatabaseAccess;
+using TitheSync.DataAccess.DTO;
+using TitheSync.Domain.Models;
+using TitheSync.Domain.Repositories;
+
+namespace TitheSync.DataAccess.Repositories
+{
+    /// <summary>
+    ///     Repository for managing member-related data access operations.
+    /// </summary>
+    /// <param name="dataAccess" >The SQL data access service.</param>
+    public class MemberRepository( ISqlDataAccess dataAccess ):IMemberRepository
+    {
+        /// <summary>
+        ///     Retrieves all members from the database.
+        /// </summary>
+        /// <returns>A collection of members.</returns>
+        public async Task<IEnumerable<Member>> GetMembersAsync()
+        {
+            // Executes a query to retrieve all members.
+            IEnumerable<MemberDto> enumerable = await dataAccess.QueryAsync<MemberDto, dynamic>("sp.Member_GetAll", new { });
+
+            // Converts a collection of MemberDto objects to a collection of Member objects.
+            return enumerable.Select(
+                x => new Member(
+                    x.MemberId,
+                    x.FirstName,
+                    x.LastName,
+                    x.Contact,
+                    x.IsLeader,
+                    x.Address,
+                    x.Organization,
+                    x.BibleClass
+                ));
+        }
+
+        /// <summary>
+        ///     Retrieves a member by their unique identifier.
+        /// </summary>
+        /// <param name="id" >The unique identifier of the member.</param>
+        /// <returns>The member if found; otherwise, null.</returns>
+        public async Task<Member?> GetMemberByIdAsync( int id )
+        {
+            // Executes a query to retrieve a member by their unique identifier.
+            IEnumerable<MemberDto> enumerable = await dataAccess.QueryAsync<MemberDto, dynamic>("sp.Member_GetById", new { MemberId = id });
+
+            // Retrieves the first member DTO from the enumerable, or null if none exist.
+            MemberDto? memberDto = enumerable.FirstOrDefault();
+
+            // Converts a MemberDto object to a Member object if the MemberDto is not null.
+            return memberDto is not null
+                ? new Member(
+                    memberDto.MemberId,
+                    memberDto.FirstName,
+                    memberDto.LastName,
+                    memberDto.Contact,
+                    memberDto.IsLeader,
+                    memberDto.Address,
+                    memberDto.Organization,
+                    memberDto.BibleClass
+                )
+                : null;
+        }
+
+
+        /// <summary>
+        ///     Adds a new member to the database.
+        /// </summary>
+        /// <param name="member" >The member to add.</param>
+        public async Task AddMemberAsync( Member member )
+        {
+            // Creates a new instance of the MemberDto class using the provided Member object.
+            MemberDto record = new(
+                member.MemberId,
+                member.FirstName,
+                member.LastName,
+                member.Contact,
+                member.IsLeader,
+                member.Address,
+                member.Organization,
+                member.BibleClass
+            );
+
+            await dataAccess.CommandAsync("sp.Member_Add", record);
+        }
+
+        /// <summary>
+        ///     Updates an existing member in the database.
+        /// </summary>
+        /// <param name="member" >The member to update.</param>
+        public async Task UpdateMemberAsync( Member member )
+        {
+            // Creates a new instance of the MemberDto class using the provided Member object.
+            MemberDto record = new(
+                member.MemberId,
+                member.FirstName,
+                member.LastName,
+                member.Contact,
+                member.IsLeader,
+                member.Address,
+                member.Organization,
+                member.BibleClass
+            );
+
+            await dataAccess.CommandAsync("sp.Member_Update", record);
+        }
+
+        /// <summary>
+        ///     Deletes a member from the database by their unique identifier.
+        /// </summary>
+        /// <param name="id" >The unique identifier of the member to delete.</param>
+        public async Task DeleteMemberAsync( int id )
+        {
+            await dataAccess.CommandAsync("sp.Member_Delete", new { MemberId = id });
+        }
+    }
+}
---
+++ TitheSync.DataAccess/Repositories/PaymentRepository.cs
@@ -0,0 +1,98 @@
+﻿using TitheSync.DataAccess.DatabaseAccess;
+using TitheSync.DataAccess.DTO;
+using TitheSync.Domain.Models;
+using TitheSync.Domain.Repositories;
+using TitheSync.Domain.Services;
+
+namespace TitheSync.DataAccess.Repositories
+{
+    /// <summary>
+    ///     Repository for managing payment-related data access operations.
+    /// </summary>
+    /// <param name="dataAccess" >The SQL data access service.</param>
+    /// <param name="dateConverterService" >The service for converting dates.</param>
+    public class PaymentRepository( ISqlDataAccess dataAccess, IDateConverterService dateConverterService ):IPaymentRepository
+    {
+        /// <summary>
+        ///     Retrieves all payments from the database.
+        /// </summary>
+        /// <returns>A collection of payments.</returns>
+        public async Task<IEnumerable<Payment>> GetPaymentsAsync()
+        {
+            // Executes a query to retrieve all payments.
+            IEnumerable<PaymentDto> enumerable = await dataAccess.QueryAsync<PaymentDto, dynamic>("sp.Payment_GetAll", new { });
+
+            // Converts a collection of PaymentDto objects to a collection of Payment objects.
+            return enumerable.Select(
+                x => new Payment(
+                    x.PaymentId,
+                    x.Amount,
+                    dateConverterService.ConvertToDateOnly(x.DatePaid)
+                ));
+        }
+
+        /// <summary>
+        ///     Retrieves a payment by its unique identifier.
+        /// </summary>
+        /// <param name="paymentId" >The unique identifier of the payment.</param>
+        /// <returns>The payment if found; otherwise, null.</returns>
+        public async Task<Payment?> GetPaymentByIdAsync( int paymentId )
+        {
+            // Executes a query to retrieve a payment by its unique identifier.
+            IEnumerable<PaymentDto> enumerable = await dataAccess.QueryAsync<PaymentDto, dynamic>("sp.Payment_GetById", new { PaymentId = paymentId });
+
+            // Retrieves the first payment DTO from the enumerable, or null if none exist.
+            PaymentDto? paymentDto = enumerable.FirstOrDefault();
+
+            // Converts a PaymentDto object to a Payment object if the PaymentDto is not null.
+            return paymentDto is not null
+                ? new Payment(
+                    paymentDto.PaymentId,
+                    paymentDto.Amount,
+                    dateConverterService.ConvertToDateOnly(paymentDto.DatePaid)
+                )
+                : null;
+        }
+
+        /// <summary>
+        ///     Adds a new payment to the database.
+        /// </summary>
+        /// <param name="payment" >The payment to add.</param>
+        public async Task AddPaymentAsync( Payment payment )
+        {
+            // Creates a new instance of the PaymentDto class using the provided Payment object.
+            PaymentDto record = new(
+                payment.PaymentId,
+                payment.Amount,
+                dateConverterService.ConvertToDateTime(payment.DatePaid)
+            );
+
+            await dataAccess.CommandAsync("sp.Payment_Add", record);
+        }
+
+        /// <summary>
+        ///     Updates an existing payment in the database.
+        /// </summary>
+        /// <param name="payment" >The payment to update.</param>
+        public async Task UpdatePaymentAsync( Payment payment )
+        {
+            // Creates a new instance of the PaymentDto class using the provided Payment object.
+            PaymentDto record = new(
+                payment.PaymentId,
+                payment.Amount,
+                dateConverterService.ConvertToDateTime(payment.DatePaid)
+            );
+
+            await dataAccess.CommandAsync("sp.Payment_Update", record);
+        }
+
+        /// <summary>
+        ///     Deletes a payment from the database by its unique identifier.
+        /// </summary>
+        /// <param name="paymentId" >The unique identifier of the payment to delete.</param>
+        public async Task DeletePaymentAsync( int paymentId )
+        {
+            await dataAccess.CommandAsync("sp.Payment_Delete", new { PaymentId = paymentId });
+        }
+    }
+}
