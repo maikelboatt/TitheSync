c6f5a71bab8eecfffa08304ee162843644564565
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-05-04 05:17:58 +0000

    Update BibleClassEnum to assign explicit integer values to enum members

--- TitheSync.Domain/Enums/BibleClassEnum.cs
+++ TitheSync.Domain/Enums/BibleClassEnum.cs
@@ -2,13 +2,13 @@
 {
     public enum BibleClassEnum
     {
-        ProfDanso,
-        MrLartey,
-        AbrahamDadzie,
-        JacobBiney,
-        AtoPrempeh,
-        MichaelKumi,
-        EmeliaAkrofi,
-        AuntyAggie
+        ProfDanso = 0,
+        MrLartey = 1,
+        EmeliaAkrofi = 2,
+        AbrahamDadzie = 3,
+        AtoPrempeh = 4,
+        MichaelKumi = 5,
+        JacobBiney = 6,
+        AuntyAggie = 7
     }
 }
