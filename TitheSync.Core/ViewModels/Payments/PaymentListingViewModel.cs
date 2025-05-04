7f4e2b61c7323340e05cf317bc3a3a1bf9dbb8a3
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-05-04 05:56:33 +0000

    Implement PaymentListingViewModel with payment service integration and methods for loading and creating payments

--- TitheSync.Core/ViewModels/Payments/PaymentListingViewModel.cs
+++ TitheSync.Core/ViewModels/Payments/PaymentListingViewModel.cs
@@ -1,8 +1,34 @@
 ﻿using MvvmCross.ViewModels;
+using TitheSync.Domain.Models;
+using TitheSync.Domain.Services;
 
 namespace TitheSync.Core.ViewModels.Payments
 {
-    public class PaymentListingViewModel:MvxViewModel
+    public class PaymentListingViewModel( IPaymentService paymentService ):MvxViewModel
     {
+        public override async Task Initialize()
+        {
+            // Initialize the base class
+            await base.Initialize();
+            await LoadAllPayments();
+            // await CreatePaymentRecord();
+        }
+
+        private async Task CreatePaymentRecord()
+        {
+            Payment payment = new(
+                0,
+                1, // Assuming a member with ID 1 exists
+                100.00m,
+                new DateOnly(2025, 04, 07)
+            );
+
+            await paymentService.AddPaymentAsync(payment);
+        }
+
+        private async Task LoadAllPayments()
+        {
+            IEnumerable<Payment> result = await paymentService.GetPaymentsAsync();
+        }
     }
 }
