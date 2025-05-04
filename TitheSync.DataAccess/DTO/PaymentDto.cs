286de945ca8a840af189bad25bb12e0acb033664
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-05-04 05:55:56 +0000

    Refactor PaymentDto to use a parameterless constructor and properties for better materialization

--- TitheSync.DataAccess/DTO/PaymentDto.cs
+++ TitheSync.DataAccess/DTO/PaymentDto.cs
@@ -1,13 +1,33 @@
 ﻿namespace TitheSync.DataAccess.DTO
 {
     /// <summary>
-    ///     Represents a payment data transfer object.
+    ///     Represents a data transfer object for a payment.
     /// </summary>
-    /// <param name="PaymentId" >The unique identifier for the payment.</param>
-    /// <param name="Amount" >The amount of the payment.</param>
-    /// <param name="DatePaid" >The date the payment was made.</param>
-    public record PaymentDto(
-        int PaymentId,
-        decimal Amount,
-        DateTime DatePaid );
+    public record PaymentDto
+    {
+        // Parameterless constructor for materialization
+        public PaymentDto() { }
+
+        /// <summary>
+        ///     Primary Constructor
+        /// </summary>
+        /// <param name="paymentId" >The unique identifier for the payment.</param>
+        /// <param name="paymentMemberId" >
+        ///     The unique identifier for the member associated with the payment.
+        /// </param>
+        /// <param name="amount" >The amount of the payment.</param>
+        /// <param name="datePaid" >The date the payment was made.</param>
+        public PaymentDto( int paymentId, int paymentMemberId, decimal amount, DateTime datePaid )
+        {
+            PaymentId = paymentId;
+            PaymentMemberId = paymentMemberId;
+            Amount = amount;
+            DatePaid = datePaid;
+        }
+
+        public int PaymentId { get; init; }
+        public int PaymentMemberId { get; init; }
+        public decimal Amount { get; init; }
+        public DateTime DatePaid { get; init; }
+    }
 }
