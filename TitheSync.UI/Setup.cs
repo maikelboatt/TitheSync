0c69763ea25cb181e4691625845119a854c51344
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-05-04 05:53:40 +0000

    Configure Serilog logging in Setup class to read from appsettings.json

--- TitheSync.UI/Setup.cs
+++ TitheSync.UI/Setup.cs
@@ -1,23 +1,27 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using MvvmCross.Platforms.Wpf.Core;
 using Serilog;
 using Serilog.Extensions.Logging;
+using System.IO;
 
 namespace TitheSync.UI
 {
     public class Setup:MvxWpfSetup<Core.App>
     {
-        protected override ILoggerProvider? CreateLogProvider()
-        {
-            return new SerilogLoggerProvider();
-        }
-        
+        protected override ILoggerProvider? CreateLogProvider() => new SerilogLoggerProvider();
+
         protected override ILoggerFactory? CreateLogFactory()
         {
+            IConfigurationRoot configuration = new ConfigurationBuilder()
+                                               .SetBasePath(Directory.GetCurrentDirectory())
+                                               .AddJsonFile("appsettings.json", false, true)
+                                               .Build();
+
             Log.Logger = new LoggerConfiguration()
-                         .MinimumLevel.Debug()
+                         .ReadFrom.Configuration(configuration)
                          .CreateLogger();
-            
+
             return new SerilogLoggerFactory();
         }
     }
