c7431eccde991b51f9e659c729238f3e94878133
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-05-04 05:54:37 +0000

    Refactor MemberDto to use properties instead of a record constructor and rename OrganizationEnums to OrganizationEnum for consistency

--- TitheSync.DataAccess/DTO/MemberDto.cs
+++ TitheSync.DataAccess/DTO/MemberDto.cs
@@ -5,21 +5,41 @@ namespace TitheSync.DataAccess.DTO
     /// <summary>
     ///     Represents a data transfer object for a member.
     /// </summary>
-    /// <param name="MemberId" >The unique identifier of the member.</param>
-    /// <param name="FirstName" >The first name of the member.</param>
-    /// <param name="LastName" >The last name of the member.</param>
-    /// <param name="Contact" >The contact information of the member.</param>
-    /// <param name="IsLeader" >Indicates whether the member is a leader.</param>
-    /// <param name="Address" >The address of the member.</param>
-    /// <param name="Organization" >The organization the member belongs to.</param>
-    /// <param name="BibleClass" >The Bible class the member attends.</param>
-    public record MemberDto(
-        int MemberId,
-        string FirstName,
-        string LastName,
-        string Contact,
-        bool IsLeader,
-        string Address,
-        OrganizationEnums Organization,
-        BibleClassEnum BibleClass );
+    public record MemberDto
+    {
+        // Constructor for materialization
+        public MemberDto() { }
+
+        /// <summary>
+        ///     Primary constructor
+        /// </summary>
+        /// <param name="memberId" >The unique identifier of the member.</param>
+        /// <param name="firstName" >The first name of the member.</param>
+        /// <param name="lastName" >The last name of the member.</param>
+        /// <param name="contact" >The contact information of the member.</param>
+        /// <param name="isLeader" >Indicates whether the member is a leader.</param>
+        /// <param name="address" >The address of the member.</param>
+        /// <param name="organization" >The organization the member belongs to.</param>
+        /// <param name="bibleClass" >The Bible class the member attends.</param>
+        public MemberDto( int memberId, string firstName, string lastName, string contact, bool isLeader, string address, OrganizationEnum organization, BibleClassEnum bibleClass )
+        {
+            MemberId = memberId;
+            FirstName = firstName;
+            LastName = lastName;
+            Contact = contact;
+            IsLeader = isLeader;
+            Address = address;
+            Organization = organization;
+            BibleClass = bibleClass;
+        }
+
+        public int MemberId { get; init; }
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
+        public string Contact { get; init; }
+        public bool IsLeader { get; init; }
+        public string Address { get; init; }
+        public OrganizationEnum Organization { get; init; }
+        public BibleClassEnum BibleClass { get; init; }
+    }
 }
