b7e4be14b2dc1805bbe02a3953b1871a77177770
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-05-04 05:56:52 +0000

    Refactor PaymentViewModel to use IViewModelFactory for creating PaymentListingViewModel instances

--- TitheSync.Core/ViewModels/PaymentViewModel.cs
+++ TitheSync.Core/ViewModels/PaymentViewModel.cs
@@ -1,10 +1,18 @@
 ﻿using MvvmCross.ViewModels;
+using TitheSync.Core.Factory;
 using TitheSync.Core.ViewModels.Payments;
 
 namespace TitheSync.Core.ViewModels
 {
-    public class PaymentViewModel:MvxViewModel, IPaymentViewModel
+    public class PaymentViewModel( IViewModelFactory viewModelFactory ):MvxViewModel, IPaymentViewModel
     {
-        public PaymentListingViewModel? PaymentListingViewModel { get; set; }
+        public PaymentListingViewModel? PaymentListingViewModel => CreateViewModel();
+
+        private PaymentListingViewModel? CreateViewModel()
+        {
+            PaymentListingViewModel? viewModel = viewModelFactory.CreateViewModel<PaymentListingViewModel>();
+            viewModel?.Initialize();
+            return viewModel;
+        }
     }
 }
