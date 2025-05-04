0ec7e536ee992865f5e8dcb5cc2ec6703132dfca
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-05-04 05:55:00 +0000

    Implement MemberListingViewModel with member management and logging functionality

--- TitheSync.Core/ViewModels/Members/MemberListingViewModel.cs
+++ TitheSync.Core/ViewModels/Members/MemberListingViewModel.cs
@@ -1,8 +1,50 @@
-﻿using MvvmCross.ViewModels;
+﻿using Microsoft.Extensions.Logging;
+using MvvmCross.ViewModels;
+using TitheSync.Domain.Enums;
+using TitheSync.Domain.Models;
+using TitheSync.Domain.Services;
 
 namespace TitheSync.Core.ViewModels.Members
 {
-    public class MemberListingViewModel:MvxViewModel
+    public class MemberListingViewModel( IMemberService memberService, ILogger<MemberListingViewModel> logger ):MvxViewModel
     {
+        private IEnumerable<Member> members;
+
+        public IEnumerable<Member> Members
+        {
+            get => members;
+            set => SetProperty(ref members, value);
+        }
+
+        public int MemberCount => Members?.Count() ?? 0;
+
+        public override async Task Initialize()
+        {
+            await base.Initialize();
+            // await CreateMemberAsync();
+            await LoadMembersAsync();
+        }
+
+        private async Task CreateMemberAsync()
+        {
+            Member newMember = new(
+                0,
+                "Nessa",
+                "Boatt",
+                "0541047330",
+                false,
+                "Methodist Street",
+                OrganizationEnum.Choir,
+                BibleClassEnum.EmeliaAkrofi);
+
+            await memberService.AddMemberAsync(newMember);
+            logger.LogInformation("Member created: {Member}", newMember);
+        }
+
+        private async Task LoadMembersAsync()
+        {
+            Members = await memberService.GetMembersAsync();
+            logger.LogInformation("Members loaded: {MemberCount}, {Members}", MemberCount, Members);
+        }
     }
 }
