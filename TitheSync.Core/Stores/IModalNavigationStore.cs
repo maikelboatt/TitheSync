6d2fb6af9bd0b4eb2b90192fbc9e528bafceb515
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-05-06 20:01:48 +0000

    Add IModalNavigationStore interface and implement ModalNavigationStore for managing modal view model navigation

---
+++ TitheSync.Core/Stores/IModalNavigationStore.cs
@@ -0,0 +1,28 @@
+﻿using MvvmCross.ViewModels;
+
+namespace TitheSync.Core.Stores
+{
+    public interface IModalNavigationStore
+    {
+        /// <summary>
+        ///     Gets or sets the current modal view model.
+        ///     Invokes the <see cref="CurrentModalViewModelChanged" /> event when set.
+        /// </summary>
+        MvxViewModel? CurrentModalViewModel { get; set; }
+
+        /// <summary>
+        ///     Indicates whether a modal view model is currently open.
+        /// </summary>
+        bool IsOpen { get; }
+
+        /// <summary>
+        ///     Event triggered when the current modal view model changes.
+        /// </summary>
+        event Action? CurrentModalViewModelChanged;
+
+        /// <summary>
+        ///     Closes the current modal by setting the view model to null.
+        /// </summary>
+        void Close();
+    }
+}
---
+++ TitheSync.Core/Stores/ModalNavigationStore.cs
@@ -0,0 +1,44 @@
+﻿using MvvmCross.ViewModels;
+
+namespace TitheSync.Core.Stores
+{
+    /// <summary>
+    ///     Manages the navigation for modal view models.
+    /// </summary>
+    public class ModalNavigationStore:IModalNavigationStore
+    {
+        private MvxViewModel? _currentModalViewModel;
+
+        /// <summary>
+        ///     Gets or sets the current modal view model.
+        ///     Invokes the <see cref="CurrentModalViewModelChanged" /> event when set.
+        /// </summary>
+        public MvxViewModel? CurrentModalViewModel
+        {
+            get => _currentModalViewModel;
+            set
+            {
+                _currentModalViewModel = value;
+                CurrentModalViewModelChanged?.Invoke();
+            }
+        }
+
+        /// <summary>
+        ///     Indicates whether a modal view model is currently open.
+        /// </summary>
+        public bool IsOpen => CurrentModalViewModel != null;
+
+        /// <summary>
+        ///     Event triggered when the current modal view model changes.
+        /// </summary>
+        public event Action? CurrentModalViewModelChanged;
+
+        /// <summary>
+        ///     Closes the current modal by setting the view model to null.
+        /// </summary>
+        public void Close()
+        {
+            CurrentModalViewModel = null;
+        }
+    }
+}
