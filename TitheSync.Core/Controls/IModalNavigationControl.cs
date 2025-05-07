3444d6d069fca784052778febc1621a898a0c5e8
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-05-07 20:15:22 +0000

    Add IModalNavigationControl interface and ModalNavigationControl class for managing modal navigation

---
+++ TitheSync.Core/Controls/IModalNavigationControl.cs
@@ -0,0 +1,22 @@
+﻿using MvvmCross.ViewModels;
+using TitheSync.Domain.Models;
+
+namespace TitheSync.Core.Controls
+{
+    public interface IModalNavigationControl
+    {
+        /// <summary>
+        ///     Displays a modal popup with the specified view model type and an optional parameter.
+        /// </summary>
+        /// <typeparam name="TViewModel" >The type of the view model to display.</typeparam>
+        /// <param name="parameter" >An optional parameter to pass to the view model.</param>
+        void PopUp<TViewModel>( int? parameter = null ) where TViewModel : IMvxViewModel;
+
+        /// <summary>
+        ///     Displays a modal popup with the specified view model type and a list of selected members.
+        /// </summary>
+        /// <typeparam name="TViewModel" >The type of the view model to display.</typeparam>
+        /// <param name="selectedMembers" >A list of members to pass to the view model.</param>
+        void PopUp<TViewModel>( List<Member> selectedMembers ) where TViewModel : IMvxViewModel;
+    }
+}
---
+++ TitheSync.Core/Controls/ModalNavigationControl.cs
@@ -0,0 +1,36 @@
+﻿using MvvmCross.ViewModels;
+using TitheSync.Core.Stores;
+using TitheSync.Domain.Models;
+
+namespace TitheSync.Core.Controls
+{
+    /// <summary>
+    ///     Represents a control for managing modal navigation.
+    /// </summary>
+    /// <param name="modalNavigationStore" >The store that manages the current modal view model.</param>
+    /// <param name="viewModelFactory" >A factory function to create view models.</param>
+    public class ModalNavigationControl( IModalNavigationStore modalNavigationStore, Func<Type, object, MvxViewModel> viewModelFactory ):IModalNavigationControl
+    {
+        /// <summary>
+        ///     Displays a modal popup with the specified view model type and an optional parameter.
+        /// </summary>
+        /// <typeparam name="TViewModel" >The type of the view model to display.</typeparam>
+        /// <param name="parameter" >An optional parameter to pass to the view model.</param>
+        public void PopUp<TViewModel>( int? parameter = null ) where TViewModel : IMvxViewModel
+        {
+            MvxViewModel viewModel = viewModelFactory(typeof(TViewModel), parameter!);
+            modalNavigationStore.CurrentModalViewModel = viewModel;
+        }
+
+        /// <summary>
+        ///     Displays a modal popup with the specified view model type and a list of selected members.
+        /// </summary>
+        /// <typeparam name="TViewModel" >The type of the view model to display.</typeparam>
+        /// <param name="selectedMembers" >A list of members to pass to the view model.</param>
+        public void PopUp<TViewModel>( List<Member> selectedMembers ) where TViewModel : IMvxViewModel
+        {
+            MvxViewModel viewModel = viewModelFactory(typeof(TViewModel), selectedMembers);
+            modalNavigationStore.CurrentModalViewModel = viewModel;
+        }
+    }
+}
