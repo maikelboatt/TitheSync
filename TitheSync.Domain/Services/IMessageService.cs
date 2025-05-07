1411a93464b680938ec82c48de3503c845d12039
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-05-07 20:14:25 +0000

    Add IMessageService interface and MessageService implementation for message display

---
+++ TitheSync.Domain/Services/IMessageService.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace TitheSync.Domain.Services
+{
+    public interface IMessageService
+    {
+        MessageBoxResult Show( string message, string caption, MessageBoxButton button, MessageBoxImage icon );
+    }
+}
---
+++ TitheSync.Domain/Services/MessageService.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows;
+
+namespace TitheSync.Domain.Services
+{
+    public class MessageService:IMessageService
+    {
+        public MessageBoxResult Show( string message, string caption, MessageBoxButton button, MessageBoxImage icon ) => MessageBox.Show(message, caption, button, icon);
+    }
+}
