27eeee326c1be334ee4b712e6743dc76485c74e0
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-05-07 20:15:36 +0000

    Add RadioButtonConverter class for binding radio button selections

---
+++ TitheSync.UI/Resources/RadioButtonConverter.cs
@@ -0,0 +1,25 @@
+﻿using System.Globalization;
+using System.Windows.Data;
+
+namespace TitheSync.UI.Resources
+{
+    public class RadioButtonConverter:IValueConverter
+    {
+        // Converts from the selected option (e.g., a string "Option1") to a bool for IsChecked
+        public object Convert( object value, Type targetType, object parameter, CultureInfo culture )
+        {
+            if (value == null || parameter == null)
+                return false;
+
+            return value.ToString().Equals(parameter.ToString(), StringComparison.InvariantCultureIgnoreCase);
+        }
+
+
+        // Converts back from IsChecked to the option value
+        public object ConvertBack( object value, Type targetType, object parameter, CultureInfo culture )
+        {
+            bool isChecked = (bool)value;
+            return isChecked ? parameter.ToString() : Binding.DoNothing;
+        }
+    }
+}
