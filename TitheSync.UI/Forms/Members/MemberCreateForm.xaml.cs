4d38f2f79ff66f7d91b6b2c03c83cf6860931c7c
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-05-07 20:16:46 +0000

    Add MemberCreateForm view for adding new members with input fields and commands

---
+++ TitheSync.UI/Forms/Members/MemberCreateForm.xaml.cs
@@ -0,0 +1,12 @@
+﻿using MvvmCross.Platforms.Wpf.Views;
+
+namespace TitheSync.UI.Forms.Members
+{
+    public partial class MemberCreateForm:MvxWpfView
+    {
+        public MemberCreateForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
