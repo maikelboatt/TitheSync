91f252e8a4818189941b5e1fb294cec7a3d962fe
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-05-07 20:13:37 +0000

    Add CustomTextBox control with data binding and validation properties

---
+++ TitheSync.UI/Resources/CustomTextBox.xaml.cs
@@ -0,0 +1,79 @@
+﻿using MvvmCross.Platforms.Wpf.Views;
+using System.Windows;
+
+namespace TitheSync.UI.Resources
+{
+    /// <summary>
+    ///     A custom WPF control that extends `MvxWpfView` and provides additional properties
+    ///     for data binding and validation.
+    /// </summary>
+    public partial class CustomTextBox:MvxWpfView
+    {
+        /// <summary>
+        ///     Dependency property for the placeholder text.
+        /// </summary>
+        public static readonly DependencyProperty PlaceholderProperty = DependencyProperty.Register(
+            nameof(Placeholder),
+            typeof(string),
+            typeof(CustomTextBox),
+            new PropertyMetadata(default(string)));
+
+        /// <summary>
+        ///     Dependency property for the description text.
+        /// </summary>
+        public static readonly DependencyProperty DescriptionProperty = DependencyProperty.Register(
+            nameof(Description),
+            typeof(string),
+            typeof(CustomTextBox),
+            new PropertyMetadata(default(string)));
+
+        /// <summary>
+        ///     Dependency property for the data value.
+        /// </summary>
+        public static readonly DependencyProperty DataProperty = DependencyProperty.Register(
+            nameof(Data),
+            typeof(string),
+            typeof(CustomTextBox),
+            new PropertyMetadata(default(string)));
+
+        /// <summary>
+        ///     Initializes a new instance of the <see cref="CustomTextBox" /> class.
+        /// </summary>
+        public CustomTextBox()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        ///     Gets or sets a value indicating whether to check if the text box is empty.
+        /// </summary>
+        public bool CheckEmpty { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the placeholder text for the text box.
+        /// </summary>
+        public string Placeholder
+        {
+            get => (string)GetValue(PlaceholderProperty);
+            set => SetValue(PlaceholderProperty, value);
+        }
+
+        /// <summary>
+        ///     Gets or sets the description text for the text box.
+        /// </summary>
+        public string Description
+        {
+            get => (string)GetValue(DescriptionProperty);
+            set => SetValue(DescriptionProperty, value);
+        }
+
+        /// <summary>
+        ///     Gets or sets the data value for the text box.
+        /// </summary>
+        public string Data
+        {
+            get => (string)GetValue(DataProperty);
+            set => SetValue(DataProperty, value);
+        }
+    }
+}
