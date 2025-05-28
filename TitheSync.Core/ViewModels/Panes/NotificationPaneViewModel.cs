108d1b49d54a8dd129bd4cd76566e1046474a4af
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-05-28 22:58:38 +0000

    Enhance NotificationPane with improved layout, notification display, and clear functionality

--- TitheSync.Core/ViewModels/Panes/NotificationPaneViewModel.cs
+++ TitheSync.Core/ViewModels/Panes/NotificationPaneViewModel.cs
@@ -1,8 +1,53 @@
-﻿using MvvmCross.ViewModels;
+﻿using MvvmCross.Commands;
+using MvvmCross.ViewModels;
+using TitheSync.Core.Models;
+using TitheSync.Core.Stores;
 
 namespace TitheSync.Core.ViewModels.Panes
 {
     public class NotificationPaneViewModel:MvxViewModel
     {
+        private readonly INotificationStore _notificationStore;
+        private MvxObservableCollection<Notification> _notifications = [];
+
+
+        public NotificationPaneViewModel( INotificationStore notificationStore )
+        {
+            _notificationStore = notificationStore;
+
+
+            _notifications.CollectionChanged += ( sender, args ) =>
+            {
+                // Handle collection changed event if needed
+                RaisePropertyChanged(() => Notifications);
+            };
+            _notificationStore.NotificationsChanged += OnNotificationsChanged;
+        }
+
+        public MvxObservableCollection<Notification> Notifications
+        {
+            get => _notifications;
+            set => SetProperty(ref _notifications, value);
+        }
+
+        public IMvxCommand ClearCommand => new MvxCommand(ClearCommandExecute);
+
+        public override Task Initialize()
+        {
+            _notifications = new MvxObservableCollection<Notification>(_notificationStore.Notifications);
+            return base.Initialize();
+        }
+
+
+        private void OnNotificationsChanged()
+        {
+            RaisePropertyChanged(() => Notifications);
+        }
+
+        private void ClearCommandExecute()
+        {
+            _notificationStore.ClearNotifications();
+            _notifications.Clear();
+        }
     }
 }
