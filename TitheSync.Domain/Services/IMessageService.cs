6580f9d66a74a613e98d5a65bc7af4dfa78750ad
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-05-28 23:04:20 +0000

    Refactored all services to the Service layer

--- TitheSync.Domain/Services/IMessageService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows;
-
-namespace TitheSync.Domain.Services
-{
-    public interface IMessageService
-    {
-        MessageBoxResult Show( string message, string caption, MessageBoxButton button, MessageBoxImage icon );
-    }
-}
--- TitheSync.Domain/Services/MessageService.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Windows;
-
-namespace TitheSync.Domain.Services
-{
-    public class MessageService:IMessageService
-    {
-        public MessageBoxResult Show( string message, string caption, MessageBoxButton button, MessageBoxImage icon ) => MessageBox.Show(message, caption, button, icon);
-    }
-}
