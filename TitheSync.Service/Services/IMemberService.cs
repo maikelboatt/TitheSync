22195d4822e9bb489cca9f4a51632acc69fd95a1
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-05-28 23:29:17 +0000

    Add IMemberService interface for member management operations

---
+++ TitheSync.Service/Services/IMemberService.cs
@@ -0,0 +1,38 @@
+﻿using TitheSync.Domain.Models;
+
+namespace TitheSync.Service.Services
+{
+    public interface IMemberService
+    {
+        /// <summary>
+        ///     Retrieves all members asynchronously.
+        /// </summary>
+        /// <returns>A collection of members.</returns>
+        Task<IEnumerable<Member>> GetMembersAsync();
+
+        /// <summary>
+        ///     Retrieves a member by their ID asynchronously.
+        /// </summary>
+        /// <param name="id" >The ID of the member to retrieve.</param>
+        /// <returns>The member with the specified ID.</returns>
+        Task<Member> GetMemberByIdAsync( int id );
+
+        /// <summary>
+        ///     Adds a new member asynchronously.
+        /// </summary>
+        /// <param name="member" >The member to add.</param>
+        Task AddMemberAsync( Member member );
+
+        /// <summary>
+        ///     Updates an existing member asynchronously.
+        /// </summary>
+        /// <param name="member" >The member to update.</param>
+        Task UpdateMemberAsync( Member member );
+
+        /// <summary>
+        ///     Deletes a member by their ID asynchronously.
+        /// </summary>
+        /// <param name="id" >The ID of the member to delete.</param>
+        Task DeleteMemberAsync( int id );
+    }
+}
