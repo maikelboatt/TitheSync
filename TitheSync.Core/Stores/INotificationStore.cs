4609ee59e9888cfdac9c35c3d3bacb0ed681c9b0
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-05-28 23:30:25 +0000

    Add INotificationStore interface and NotificationStore class for notification management

---
+++ TitheSync.Core/Stores/INotificationStore.cs
@@ -0,0 +1,15 @@
+﻿using TitheSync.Core.Models;
+
+namespace TitheSync.Core.Stores
+{
+    public interface INotificationStore
+    {
+        List<Notification> Notifications { get; set; }
+
+        void AddNotification( Notification notification );
+
+        void ClearNotifications();
+
+        event Action? NotificationsChanged;
+    }
+}
---
+++ TitheSync.Core/Stores/NotificationStore.cs
@@ -0,0 +1,39 @@
+﻿using TitheSync.Core.Models;
+
+namespace TitheSync.Core.Stores
+{
+    /// <summary>
+    ///     Stores and manages notifications.
+    /// </summary>
+    public class NotificationStore:INotificationStore
+    {
+        /// <summary>
+        ///     Gets or sets the list of notifications.
+        /// </summary>
+        public List<Notification> Notifications { get; set; } = [];
+
+        /// <summary>
+        ///     Adds a notification to the store.
+        /// </summary>
+        /// <param name="notification" >The notification to add.</param>
+        public void AddNotification( Notification notification )
+        {
+            Notifications.Add(notification);
+            NotificationsChanged?.Invoke();
+        }
+
+        /// <summary>
+        ///     Clears all notifications from the store.
+        /// </summary>
+        public void ClearNotifications()
+        {
+            Notifications.Clear();
+            NotificationsChanged?.Invoke();
+        }
+
+        /// <summary>
+        ///     Event triggered when notifications change.
+        /// </summary>
+        public event Action? NotificationsChanged;
+    }
+}
