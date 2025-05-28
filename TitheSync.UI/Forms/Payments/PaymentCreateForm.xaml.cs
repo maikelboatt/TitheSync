0a13b5f18abc005e077d5d5086045607c08d678e
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-05-28 23:33:52 +0000

    Add PaymentCreateForm for creating new payment records

---
+++ TitheSync.UI/Forms/Payments/PaymentCreateForm.xaml.cs
@@ -0,0 +1,12 @@
+﻿using MvvmCross.Platforms.Wpf.Views;
+
+namespace TitheSync.UI.Forms.Payments
+{
+    public partial class PaymentCreateForm:MvxWpfView
+    {
+        public PaymentCreateForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
