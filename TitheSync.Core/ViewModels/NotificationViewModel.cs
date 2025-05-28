5113cd22245a3443bf9c5ecfe2f8da4c7341777e
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-05-28 22:58:54 +0000

    Refactor NotificationViewModel to use IViewModelFactory for creating NotificationPaneViewModel

--- TitheSync.Core/ViewModels/NotificationViewModel.cs
+++ TitheSync.Core/ViewModels/NotificationViewModel.cs
@@ -1,10 +1,18 @@
 ﻿using MvvmCross.ViewModels;
+using TitheSync.Core.Factory;
 using TitheSync.Core.ViewModels.Panes;
 
 namespace TitheSync.Core.ViewModels
 {
-    public class NotificationViewModel:MvxViewModel
+    public class NotificationViewModel( IViewModelFactory viewModelFactory ):MvxViewModel
     {
-        public NotificationPaneViewModel? NotificationPaneViewModel { get; set; }
+        public NotificationPaneViewModel? NotificationPaneViewModel => CreateViewModel();
+
+        public NotificationPaneViewModel? CreateViewModel()
+        {
+            NotificationPaneViewModel? viewModel = viewModelFactory.CreateViewModel<NotificationPaneViewModel>();
+            viewModel?.Initialize();
+            return viewModel;
+        }
     }
 }
