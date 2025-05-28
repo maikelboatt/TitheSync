4a37c9dcb3eacb851e4777242c94a9e187b335f0
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-05-28 23:35:20 +0000

    Add NavigationParameter class for managing navigation parameters

---
+++ TitheSync.Core/Parameters/NavigationParameter.cs
@@ -0,0 +1,9 @@
+﻿namespace TitheSync.Core.Parameters
+{
+    public class NavigationParameter
+    {
+        public int Id { get; set; }
+
+        public Type? CallingViewModel { get; set; }
+    }
+}
