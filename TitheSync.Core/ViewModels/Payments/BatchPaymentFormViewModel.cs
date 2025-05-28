1a73b768165af20aabd9b8a93ec7a3da5a643ce9
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-05-28 22:48:27 +0000

    Add BatchPaymentForm and its ViewModel for processing batch payments

---
+++ TitheSync.Core/ViewModels/Payments/BatchPaymentFormViewModel.cs
@@ -0,0 +1,13 @@
+﻿using MvvmCross.ViewModels;
+using TitheSync.Domain.Models;
+
+namespace TitheSync.Core.ViewModels.Payments
+{
+    public class BatchPaymentFormViewModel:MvxViewModel<Member>, IBatchPaymentFormViewModel
+    {
+        public override void Prepare( Member parameter )
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
