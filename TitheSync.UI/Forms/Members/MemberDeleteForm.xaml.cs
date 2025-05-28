1cd1988c26c172cb95fdfa9f9f1528f2d7306ca8
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-05-28 23:30:07 +0000

    Add MemberDeleteForm for member deletion functionality

---
+++ TitheSync.UI/Forms/Members/MemberDeleteForm.xaml.cs
@@ -0,0 +1,12 @@
+﻿using MvvmCross.Platforms.Wpf.Views;
+
+namespace TitheSync.UI.Forms.Members
+{
+    public partial class MemberDeleteForm:MvxWpfView
+    {
+        public MemberDeleteForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
