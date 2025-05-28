805b162c992d7fe55d41a1810415c3c51c405493
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-05-28 23:34:29 +0000

    Add PaymentDeleteForm for managing payment deletion

---
+++ TitheSync.UI/Forms/Payments/PaymentDeleteForm.xaml.cs
@@ -0,0 +1,12 @@
+﻿using MvvmCross.Platforms.Wpf.Views;
+
+namespace TitheSync.UI.Forms.Payments
+{
+    public partial class PaymentDeleteForm:MvxWpfView
+    {
+        public PaymentDeleteForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
