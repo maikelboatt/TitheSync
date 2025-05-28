59a492e9c85e85a1b147f03a44f742db8e17b372
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-05-28 23:28:29 +0000

    Add AggregatedPayment model to represent member payment data

---
+++ TitheSync.Core/Models/AggregatedPayment.cs
@@ -0,0 +1,11 @@
+﻿namespace TitheSync.Core.Models
+{
+    public class AggregatedPayment
+    {
+        public int MemberId { get; set; }
+        public string MemberName { get; set; }
+        public decimal TotalAmount { get; set; }
+
+        public string CurrentMonth { get; set; } = string.Empty;
+    }
+}
