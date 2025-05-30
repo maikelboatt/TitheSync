5938b28c4e38c267f154adeb6c60bca10e7c0d56
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-05-30 00:54:12 +0000

    Add IReportingService interface and ReportingService implementation

---
+++ TitheSync.Business/Services/Reports/IReportingService.cs
@@ -0,0 +1,12 @@
+﻿using TitheSync.Domain.Enums;
+using TitheSync.Domain.Models;
+
+namespace TitheSync.Business.Services.Reports
+{
+    public interface IReportingService
+    {
+        Task<PaymentWithName> GetAllPaymentsForBibleClass( BibleClassEnum bibleClass, DateOnly startDate, DateOnly endDate );
+
+        Task<PaymentWithName> GetAllPaymentsOfMember( DateOnly startDate, DateOnly endDate );
+    }
+}
---
+++ TitheSync.Business/Services/Reports/ReportingService.cs
@@ -0,0 +1,14 @@
+﻿using TitheSync.ApplicationState.Stores.Members;
+using TitheSync.ApplicationState.Stores.Payments;
+using TitheSync.Domain.Enums;
+using TitheSync.Domain.Models;
+
+namespace TitheSync.Business.Services.Reports
+{
+    public class ReportingService( IMemberStore memberStore, IPaymentStore paymentStore ):IReportingService
+    {
+        public Task<PaymentWithName> GetAllPaymentsForBibleClass( BibleClassEnum bibleClass, DateOnly startDate, DateOnly endDate ) => throw new NotImplementedException();
+
+        public Task<PaymentWithName> GetAllPaymentsOfMember( DateOnly startDate, DateOnly endDate ) => throw new NotImplementedException();
+    }
+}
