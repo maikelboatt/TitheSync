8108458f00b2aac40c387c9340a0b1ef80fb18fc
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-06-04 21:11:14 +0000

    Add ThemesController for managing light and dark theme switching

---
+++ TitheSync.UI/Themes/ThemesController.cs
@@ -0,0 +1,45 @@
+﻿using System.Windows;
+
+namespace TitheSync.UI.Themes
+{
+    public class ThemesController
+    {
+        public static ThemeTypes CurrentTheme { get; set; }
+        public static ResourceDictionary ThemeDictionary
+        {
+            get => Application.Current.Resources.MergedDictionaries[0];
+            set => Application.Current.Resources.MergedDictionaries[0] = value;
+        }
+
+        private static void ChangeTheme( Uri uri )
+        {
+            ThemeDictionary = new ResourceDictionary { Source = uri };
+        }
+
+        public static void SetTheme( ThemeTypes theme )
+        {
+            string themeName = null!;
+            CurrentTheme = theme;
+
+            themeName = theme switch
+            {
+                ThemeTypes.Light => "LightTheme",
+                ThemeTypes.Dark  => "DarkTheme",
+                _                => themeName
+            };
+
+            try
+            {
+                if (!string.IsNullOrEmpty(themeName))
+                {
+                    ChangeTheme(new Uri($"Themes/{themeName}.xaml", UriKind.Relative));
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+        }
+    }
+}
