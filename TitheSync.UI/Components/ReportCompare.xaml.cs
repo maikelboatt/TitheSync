20a77d7de481ecb926535fbc3dd86ef47abdc61e
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-06-11 01:19:57 +0000

    Add ReportCompare view and code-behind for report comparison functionality

---
+++ TitheSync.UI/Components/ReportCompare.xaml.cs
@@ -0,0 +1,12 @@
+﻿using MvvmCross.Platforms.Wpf.Views;
+
+namespace TitheSync.UI.Components
+{
+    public partial class ReportCompare:MvxWpfView
+    {
+        public ReportCompare()
+        {
+            InitializeComponent();
+        }
+    }
+}
