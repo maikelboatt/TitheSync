fb5212eaa7f239c61443e87f57dcfe02388f08f8
maikelboatt <107644570+maikelboatt@users.noreply.github.com>
2025-06-12 01:57:44 +0000

    Add IReportStore interface and ReportStore implementation for payment reports

---
+++ TitheSync.ApplicationState/Stores/Reports/IReportStore.cs
@@ -0,0 +1,40 @@
+﻿using TitheSync.Domain.Enums;
+
+namespace TitheSync.ApplicationState.Stores.Reports
+{
+    /// <summary>
+    ///     Provides aggregated payment reports for members and Bible classes.
+    /// </summary>
+    public interface IReportStore
+    {
+        /// <summary>
+        ///     Gets or sets the total quarter payments for each member.
+        /// </summary>
+        IEnumerable<(string FullName, decimal TotalAmount)> QuarterPaymentsForMembers { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the total semi-annual payments for each member.
+        /// </summary>
+        IEnumerable<(string FullName, decimal TotalAmount)> SemiAnnualPaymentsForMembers { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the total annual payments for each member.
+        /// </summary>
+        IEnumerable<(string FullName, decimal TotalAmount)> AnnualPaymentsForMembers { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the total quarter payments for each Bible class.
+        /// </summary>
+        IEnumerable<(BibleClassEnum BibleClass, decimal TotalAmount)> QuarterPaymentsForBibleClasses { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the total semi-annual payments for each Bible class.
+        /// </summary>
+        IEnumerable<(BibleClassEnum BibleClass, decimal TotalAmount)> SemiAnnualPaymentsForBibleClasses { get; set; }
+
+        /// <summary>
+        ///     Gets or sets the total annual payments for each Bible class.
+        /// </summary>
+        IEnumerable<(BibleClassEnum BibleClass, decimal TotalAmount)> AnnualPaymentsForBibleClasses { get; set; }
+    }
+}
---
+++ TitheSync.ApplicationState/Stores/Reports/ReportStore.cs
@@ -0,0 +1,14 @@
+﻿using TitheSync.Domain.Enums;
+
+namespace TitheSync.ApplicationState.Stores.Reports
+{
+    public class ReportStore:IReportStore
+    {
+        public IEnumerable<(string FullName, decimal TotalAmount)> QuarterPaymentsForMembers { get; set; }
+        public IEnumerable<(string FullName, decimal TotalAmount)> SemiAnnualPaymentsForMembers { get; set; }
+        public IEnumerable<(string FullName, decimal TotalAmount)> AnnualPaymentsForMembers { get; set; }
+        public IEnumerable<(BibleClassEnum BibleClass, decimal TotalAmount)> QuarterPaymentsForBibleClasses { get; set; }
+        public IEnumerable<(BibleClassEnum BibleClass, decimal TotalAmount)> SemiAnnualPaymentsForBibleClasses { get; set; }
+        public IEnumerable<(BibleClassEnum BibleClass, decimal TotalAmount)> AnnualPaymentsForBibleClasses { get; set; }
+    }
+}
